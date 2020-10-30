510d607980e212623bce3770289960111f19e6fc
Adrian <adfpizarro@gmail.com>
2020-10-30 13:00:22 -0700

    Prevent to friend youtself

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,6 +8,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    if current_user.id==params[:id].to_i
+    @amI=true
+    else
+    @amI=false
+    end    
+    
+
     @posts = @user.posts.ordered_by_most_recent
   end
 end
