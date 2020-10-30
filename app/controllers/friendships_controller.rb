a26828bda7dd9a6ebfce6995f7255e7ac7260356
Adrian <adfpizarro@gmail.com>
2020-10-30 12:02:45 -0700

    Add method to create friendrequest from the user profile
    Add resource user-friendship

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -6,6 +6,20 @@ class FriendshipsController < ApplicationController
     @pending_friends = current_user.pending_friends
     @friend_requests =  current_user.friend_requests
   end
+   
+  def create
+    request=Friendship.new
+    request.user_id=current_user.id
+    request.friend_id=(params[:user_id])
+    p "-------------- Request data -----------"
+    p request
+    request.save
+  end  
+
+  def update
+   friend=User.find(params[:id])
+   current_user.confirm_friend(friend)
+  end 
 
   def show
     @user = User.find(params[:id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,7 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @nRequesT=Friendship.new
   end
 
   def show
