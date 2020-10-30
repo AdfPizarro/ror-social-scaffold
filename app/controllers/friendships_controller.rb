778889518a1508bad92ce675a12df0889f7c0f46
Adrian <adfpizarro@gmail.com>
2020-10-30 13:38:18 -0700

    Add method to confirm and reject friendship request

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -5,27 +5,28 @@ class FriendshipsController < ApplicationController
     @friends = User.all
     @pending_friends = current_user.pending_friends
     @friend_requests =  current_user.friend_requests
+    p @friend_requests
   end
    
   def create
     request=Friendship.new
     request.user_id=current_user.id
     request.friend_id=(params[:user_id])
-    p "-------------- Request data -----------"
-    p request
+    request.confirmed=false
     request.save
   end  
 
   def destroy
-  p "------------ Cancel friendship"
-  request=a=Friendship.find_by(user_id:current_user.id,friend_id:params[:id])
+  request=Friendship.find(params[:id])
   request.destroy
   redirect_to(friendships_path, alert: 'Frienship request deleted')
   end
 
   def update
-   friend=User.find(params[:id])
-   current_user.confirm_friend(friend)
+   request=Friendship.find(params[:id])
+   request.confirmed=true
+   request.save
+   redirect_to(friendships_path, alert: 'Friendship confirmed')
   end 
 
   def show
