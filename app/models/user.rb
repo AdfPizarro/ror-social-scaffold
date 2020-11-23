f7dc894a21a12dbe22faf5f109fdcbd7eee35e9e
Adrian <adfpizarro@gmail.com>
2020-11-23 14:13:31 -0700

    Switch method to get freidns to active record instead ruby

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,11 +11,8 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
-
-  def friends
-    friends_array = friendships.map { |friendship| friendship.friend if friendship.confirmed }
-    friends_array.compact
-  end
+  has_many :confirmed_friendships, -> { where confirmed: true }, class_name: "Friendship"
+  has_many :friends, through: :confirmed_friendships  
 
   def pending_friends
     friendships.map { |friendship| friendship.friend unless friendship.confirmed }.compact
